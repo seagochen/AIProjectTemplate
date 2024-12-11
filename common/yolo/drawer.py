7c9484448f783a222cf9a346e0ff6d9dabd5c0a2
Orlando Chen <seagochen@hotmail.com>
2024-12-11 11:40:26 +0900

    ソースコードのバグを修正された。

--- common/yolo/drawer.py
+++ common/yolo/drawer.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Union, Tuple
 
 import cv2
 import numpy as np
@@ -17,160 +17,106 @@ class Drawer:
         self.bbox_colors = schema_loader.bbox_colors
 
 
-    def draw_skeletons_with_bboxes(self, image: np.ndarray, results: List[Union[YoloPose, YoloPoseSorted]],
-                                   different_bbox: bool = False, show_pts: bool = True,
-                                   show_names: bool = True) -> np.ndarray:
+    @staticmethod
+    def draw_color_bbox(image: np.ndarray, 
+                        text: str, text_color: Tuple[int, int, int],
+                        bbox_coords: Tuple[int, int, int, int], bbox_color: Tuple[int, int, int]):
+
+        # 绘制边界框
+        cv2.rectangle(image, (bbox_coords[0], bbox_coords[1]),
+                      (bbox_coords[2], bbox_coords[3]), bbox_color, 1)
+
+        # 绘制文字
+        if text is not None:
+            # 获取文字大小
+            (text_width, text_height), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+
+            # 绘制信息背景
+            cv2.rectangle(image, (bbox_coords[0], bbox_coords[1] - text_height - 5),
+                          (bbox_coords[0] + text_width, bbox_coords[1]), bbox_color, -1)
+
+            # 显示文字
+            cv2.putText(image, text, (bbox_coords[0], bbox_coords[1] - 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1)
+
+
+    def draw_skeletons(self, frame: np.ndarray, results: List[Union[YoloPose, YoloPoseSorted]],
+                       conf_threshold: float = 0.5, bbox_style: str = "normal",
+                       show_skeletons: bool = True, show_pts: bool = True,
+                       show_pts_name: bool = True) -> np.ndarray:
         """
-        在图像上绘制骨骼和边界框。
+        在图像上绘制骨骼。
 
-        :param image: cv2 图像
-        :param results: YoloPose 或 YoloPoseSorted 对象列表
-        :param different_bbox: 是否使用不同颜色的边界框
-        :param show_pts: 是否显示关键点
-        :param show_names: 是否显示关键点名称
-        :return: 绘制后的图像
+        Args:
+            frame: cv2 图像
+            results: YoloPose 或 YoloPoseSorted 对象列表
+            conf_threshold: 置信度阈值
+            bbox_style: 边界框风格，可选值：
+
+            - "normal" 所有被识别到的人物都绘制蓝色边界框，
+            - "chromatic" 当results为YoloPoseSorted时，根据id绘制不同颜色的边界框
+            - "none" 不绘制边界框
+
+            show_skeletons: 是否绘制骨骼
+            show_pts: 是否绘制关键点
+            show_pts_name: 是否显示关键点名称
+
+        Returns:
+            绘制后的图像
         """
-        for idx, pose in enumerate(results):
-            # 绘制关键点
-            if show_pts:
-                for i, pt in enumerate(pose.pts):
-                    if pt.conf > 0.2 and i in self.kpt_color_map:
-                        kp = self.kpt_color_map[i]
-                        cv2.circle(image, (pt.x, pt.y), 3, kp.color, -1)
 
-                        if show_names:
-                            cv2.putText(image, kp.name, (pt.x, pt.y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-                                        0.5, kp.color, 1)
+        frame_copy = frame.copy()
 
-            # 绘制骨骼
-            for bone in self.skeleton_map:
-                srt_kp = pose.pts[bone.srt_kpt_id]
-                dst_kp = pose.pts[bone.dst_kpt_id]
-
-                if all([
-                    srt_kp.conf > 0.2,
-                    dst_kp.conf > 0.2,
-                    srt_kp.x > 0, srt_kp.y > 0,
-                    dst_kp.x > 0, dst_kp.y > 0
-                ]):
-                    cv2.line(image, (srt_kp.x, srt_kp.y), (dst_kp.x, dst_kp.y), bone.color, 2)
-
-            # 确定边界框颜色
-            if different_bbox:
-                box_color = self.bbox_colors[idx % len(self.bbox_colors)]
-            else:
-                box_color = (255, 0, 0)  # 默认蓝色
-
-            # 绘制边界框
-            lx, ly, rx, ry = pose.lx, pose.ly, pose.rx, pose.ry
-            cv2.rectangle(image, (lx, ly), (rx, ry), box_color, 2)
-
-            # 创建标签文本
-            label = f"Person: {pose.conf:.2f}"
-
-            # 获取文本大小
-            text_size, baseline = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-
-            # 绘制文本背景
-            cv2.rectangle(image, (lx, ly - text_size[1] - 5), (lx + text_size[0], ly), box_color, cv2.FILLED)
-
-            # 绘制标签文本
-            cv2.putText(image, label, (lx, ly - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-
-        return image
-
-
-    def draw_skeletons_without_bboxes(self, image: np.ndarray, results: List[Union[YoloPose, YoloPoseSorted]],
-                                      show_pts: bool = True, show_names: bool = True) -> np.ndarray:
-        """
-        在图像上绘制骨骼，不包括边界框。
-
-        :param image: cv2 图像
-        :param results: YoloPose 或 YoloPoseSorted 对象列表
-        :param show_pts: 是否显示关键点
-        :param show_names: 是否显示关键点名称
-        :return: 绘制后的图像
-        """
         for pose in results:
-            # 绘制关键点
-            if show_pts:
-                for i, pt in enumerate(pose.pts):
-                    if pt.conf > 0.2 and i in self.kpt_color_map:
-                        kp = self.kpt_color_map[i]
-                        cv2.circle(image, (pt.x, pt.y), 3, kp.color, -1)
 
-                        if show_names:
-                            cv2.putText(image, kp.name, (pt.x, pt.y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-                                        0.5, kp.color, 1)
+            # 首先绘制关键点
+            if show_pts:
+                for idx, kpt in enumerate(pose.pts):
+                    if kpt.conf > conf_threshold and idx in self.kpt_color_map:
+                        kp = self.kpt_color_map[idx]
+                        cv2.circle(frame_copy, (int(kpt.x), int(kpt.y)), 3, kp.color, -1)
+
+                        # 显示关键点名称
+                        if show_pts_name:
+                            cv2.putText(frame_copy, kp.name, (int(kpt.x), int(kpt.y - 5)),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, kp.color, 1)
 
             # 绘制骨骼
-            for bone in self.skeleton_map:
-                srt_kp = pose.pts[bone.srt_kpt_id]
-                dst_kp = pose.pts[bone.dst_kpt_id]
+            if show_skeletons:
+                for bone in self.skeleton_map:
+                    srt_kp = pose.pts[bone.srt_kpt_id]
+                    dst_kp = pose.pts[bone.dst_kpt_id]
 
-                if all([
-                    srt_kp.conf > 0.2,
-                    dst_kp.conf > 0.2,
-                    srt_kp.x > 0, srt_kp.y > 0,
-                    dst_kp.x > 0, dst_kp.y > 0
-                ]):
-                    cv2.line(image, (srt_kp.x, srt_kp.y), (dst_kp.x, dst_kp.y), bone.color, 2)
+                    if all([
+                        srt_kp.conf > conf_threshold,
+                        dst_kp.conf > conf_threshold,
+                        srt_kp.x > 0, srt_kp.y > 0,
+                        dst_kp.x > 0, dst_kp.y > 0
+                    ]):
+                        cv2.line(frame_copy, (int(srt_kp.x), int(srt_kp.y)),
+                                 (int(dst_kp.x), int(dst_kp.y)), bone.color, 1)
 
-        return image
-
-
-    def draw_bboxes_with_labels(self, image: np.ndarray, results: List[Union[Yolo, YoloSorted]],
-                                labels: List[str]) -> np.ndarray:
-        """
-        在图像上绘制带标签的边界框。
-
-        :param image: cv2 图像
-        :param results: Yolo 或 YoloSorted 对象列表
-        :param labels: 类别标签列表
-        :return: 绘制后的图像
-        """
-        for yolo in results:
-            lx, ly, rx, ry, cls, conf = yolo.lx, yolo.ly, yolo.rx, yolo.ry, yolo.cls, yolo.conf
-
-            # 根据类别选择颜色
-            box_color = self.bbox_colors[cls % len(self.bbox_colors)]
 
             # 绘制边界框
-            cv2.rectangle(image, (lx, ly), (rx, ry), box_color, 2)
+            if bbox_style == "normal":
 
-            # 创建标签文本
-            label = f"{labels[cls]}: {conf:.2f}"
+                # 创建文字
+                text = f"{pose.conf:.2f}"
 
-            # 获取文本大小
-            text_size, baseline = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+                # 绘制信息背景
+                self.draw_color_bbox(frame_copy, text, (255, 255, 255),
+                                     (pose.lx, pose.ly, pose.rx, pose.ry), (255, 0, 0))
 
-            # 绘制文本背景
-            cv2.rectangle(image, (lx, ly - text_size[1] - 5), (lx + text_size[0], ly), box_color, cv2.FILLED)
+            elif bbox_style == "chromatic":
 
-            # 绘制标签文本
-            cv2.putText(image, label, (lx, ly - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                # 创建文字
+                text = f"{pose.oid}: {pose.conf:.2f}"
 
-        return image
+                # 绘制信息背景
+                self.draw_color_bbox(frame_copy, text, (255, 255, 255),
+                                     (pose.lx, pose.ly, pose.rx, pose.ry), self.bbox_colors[pose.oid % len(self.bbox_colors)])
 
-
-    def draw_bboxes_without_labels(self, image: np.ndarray, results: List[Union[Yolo, YoloSorted]]) -> np.ndarray:
-        """
-        在图像上绘制不带标签的边界框。
-
-        :param image: cv2 图像
-        :param results: Yolo 或 YoloSorted 对象列表
-        :return: 绘制后的图像
-        """
-        for yolo in results:
-            lx, ly, rx, ry, cls, conf = yolo.lx, yolo.ly, yolo.rx, yolo.ry, yolo.cls, yolo.conf
-
-            # 根据类别选择颜色
-            box_color = self.bbox_colors[cls % len(self.bbox_colors)]
-
-            # 绘制边界框
-            cv2.rectangle(image, (lx, ly), (rx, ry), box_color, 2)
-
-        return image
+        return frame_copy
 
 
     def draw_facial_vectors_2d(self, frame: np.ndarray, orientation_vectors: List[FacialOrientation2D],
@@ -184,6 +130,8 @@ class Drawer:
         :param show_legend: 是否显示图例
         :return: 绘制后的图像
         """
+        frame_copy = frame.copy()
+
         for idx, vector in enumerate(orientation_vectors):
             # 确定向量颜色
             if different_vectors:
@@ -192,13 +140,57 @@ class Drawer:
                 vector_color = (255, 0, 0)  # 默认蓝色
 
             # 绘制箭头
-            cv2.arrowedLine(frame, (vector.origin_x, vector.origin_y), (vector.dest_x, vector.dest_y),
+            cv2.arrowedLine(frame_copy, (vector.origin_x, vector.origin_y), (vector.dest_x, vector.dest_y),
                             vector_color, 2)
 
             # 绘制图例
             if show_legend:
                 face_direction = str(vector)
-                cv2.putText(frame, face_direction, (vector.origin_x + 5, vector.origin_y + 15),
+                cv2.putText(frame_copy, face_direction, (vector.origin_x + 5, vector.origin_y + 15),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, vector_color, 1)
 
-        return frame
+        return frame_copy
+
+
+    def draw_objects(self, frame: np.ndarray, results: List[Union[Yolo, YoloSorted]], 
+                     labels: List[Union[str, int]] = None,
+                     conf_threshold: float = 0.5, bbox_style: str = "normal") -> np.ndarray:
+        """
+        在图像上绘制边界框。
+        """
+        frame_copy = frame.copy()
+
+        for obj in results:
+
+            # bboxの色を決定
+            if bbox_style == "normal":
+                bbox_color = (255, 0, 0)
+            elif bbox_style == "chromatic":
+                bbox_color = self.bbox_colors[obj.oid % len(self.bbox_colors)]
+            elif bbox_style == "by_class":
+                bbox_color = self.bbox_colors[obj.cls % len(self.bbox_colors)]
+            else:
+                bbox_color = (255, 0, 0)
+
+            # 绘制边界框
+            if obj.conf > conf_threshold:
+                if labels is not None and obj.cls < len(labels):
+                    text = f"{labels[obj.cls]}: {obj.conf:.2f} - {obj.oid}"
+                    self.draw_color_bbox(frame_copy, text, (255, 255, 255),
+                                        (obj.lx, obj.ly, obj.rx, obj.ry), bbox_color)
+                
+                elif labels is not None and obj.cls >= len(labels):
+                    text = f"Unknown: {obj.conf:.2f} - {obj.oid}"
+                    self.draw_color_bbox(frame_copy, text, (255, 255, 255),
+                                        (obj.lx, obj.ly, obj.rx, obj.ry), bbox_color)
+                
+                elif labels is None and bbox_style == "chromatic":
+                    text = f"{obj.oid}: {obj.conf:.2f}"
+                    self.draw_color_bbox(frame_copy, text, (255, 255, 255),
+                                        (obj.lx, obj.ly, obj.rx, obj.ry), bbox_color)
+
+                else:
+                    self.draw_color_bbox(frame_copy, None, (255, 255, 255),
+                                        (obj.lx, obj.ly, obj.rx, obj.ry), bbox_color)
+
+        return frame_copy
